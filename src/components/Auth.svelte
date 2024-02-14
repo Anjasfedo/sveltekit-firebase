f0e82fab6aff8d493ae47ae3046825f4485e0eb9
Anjasfedo <fedoaf98@gmail.com>
2024-02-14 20:14:45 +0700

    Layout of Auth Component

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -1,8 +1,50 @@
-<div class="flex flex-col items-center">
-  <form action="">
-    <h1>Login</h1>
-    <label for=""><input type="text" placeholder="email" /></label>
-    <label for=""><input type="text" placeholder="email" /></label>
-    <label for=""><input type="text" placeholder="email" /></label>
+<script>
+  let email = "";
+  let password = "";
+  let confirmPassword = "";
+</script>
+
+<div class="flex flex-col items-center justify-center flex-1 p-1">
+  <form class="flex flex-col gap-2 max-w-full w-[400px] mx-auto">
+    <h1 class="text-center text-5xl font-bold p-2">Login</h1>
+    <label
+      for=""
+      class="relative border-solid border-2 border-lime-300 rounded-lg"
+      >
+      <p class={"absolute translate-y-5 pointer-events-none text-white rounded-sm font-light" + email ? " top-0 left-[24px] border-[1px] border-solid border-lime-300" : " top top-[50%] left-[4px] border-2 border-solid border-transparent opacity-0"}>Email</p>
+      <input
+        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-3"
+        type="email"
+        placeholder="Email"
+        bind:value={email}
+      /></label
+    >
+    <label
+      for=""
+      class="relative border-solid border-2 border-lime-300 rounded-lg"
+      >
+      <p class={password ? " " : " "}>Password</p><input
+        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-3"
+        type="password"
+        placeholder="Password"
+        bind:value={password}
+      /></label
+    >
+    <label
+      for=""
+      class="relative border-solid border-2 border-lime-300 rounded-lg"
+      >
+      <p class={confirmPassword ? " " : " "}>ConfirmPassword</p><input
+        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-3"
+        type="password"
+        placeholder="Confirm Password"
+        bind:value={confirmPassword}
+      /></label
+    >
+    <button
+      type="button"
+      class="bg-lime-400 text-white border-none px-3 py-2 cursor-pointer rounded-md hover:bg-lime-600"
+      >Submit</button
+    >
   </form>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import "../app.css";
 </script>
 
-<div class="min-h-screen bg-gradient-to-tr from-zinc-700 to-sky-950 text-white relative flex flex-col">
+<div class="min-h-screen bg-gradient-to-tr from-zinc-700 to-sky-950 text-white relative flex flex-col duration-200">
   <slot />
 </div>
