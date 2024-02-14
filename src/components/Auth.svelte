6fb043be4d3583457777cc84f1230850693171b5
Anjasfedo <fedoaf98@gmail.com>
2024-02-14 20:22:17 +0700

    Conditional Class on Paragraph

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -5,15 +5,25 @@
 </script>
 
 <div class="flex flex-col items-center justify-center flex-1 p-1">
-  <form class="flex flex-col gap-2 max-w-full w-[400px] mx-auto">
+  <form class="flex flex-col gap-6 max-w-full w-[400px] mx-auto">
     <h1 class="text-center text-5xl font-bold p-2">Login</h1>
     <label
       for=""
       class="relative border-solid border-2 border-lime-300 rounded-lg"
+    >
+      <p
+        class="
+  absolute -translate-y-[50%] pointer-events-none text-white rounded-sm font-light
+  {email
+          ? 'top-0 left-[24px] border-[1px] border-solid border-lime-300 bg-slate-900 rounded-lg px-1'
+          : 'top-[50%] left-[4px] border-2 border-solid border-transparent opacity-0'}
+"
       >
-      <p class={"absolute translate-y-5 pointer-events-none text-white rounded-sm font-light" + email ? " top-0 left-[24px] border-[1px] border-solid border-lime-300" : " top top-[50%] left-[4px] border-2 border-solid border-transparent opacity-0"}>Email</p>
+        Email
+      </p>
+
       <input
-        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-3"
+        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-4"
         type="email"
         placeholder="Email"
         bind:value={email}
@@ -22,9 +32,19 @@
     <label
       for=""
       class="relative border-solid border-2 border-lime-300 rounded-lg"
+    >
+      <p
+        class="
+      absolute -translate-y-[50%] pointer-events-none text-white rounded-sm font-light
+      {password
+          ? 'top-0 left-[24px] border-[1px] border-solid border-lime-300 bg-slate-900 rounded-lg px-1'
+          : 'top-[50%] left-[4px] border-2 border-solid border-transparent opacity-0'}
+    "
       >
-      <p class={password ? " " : " "}>Password</p><input
-        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-3"
+        Password
+      </p>
+      <input
+        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-4"
         type="password"
         placeholder="Password"
         bind:value={password}
@@ -33,9 +53,19 @@
     <label
       for=""
       class="relative border-solid border-2 border-lime-300 rounded-lg"
+    >
+      <p
+        class="
+      absolute -translate-y-[50%] pointer-events-none text-white rounded-sm font-light
+      {confirmPassword
+          ? 'top-0 left-[24px] border-[1px] border-solid border-lime-300 bg-slate-900 rounded-lg px-1'
+          : 'top-[50%] left-[4px] border-2 border-solid border-transparent opacity-0'}
+    "
       >
-      <p class={confirmPassword ? " " : " "}>ConfirmPassword</p><input
-        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-3"
+        ConfirmPassword
+      </p>
+      <input
+        class="w-full border-none bg-transparent text-white focus:border-none focus:outline-none px-2 py-4"
         type="password"
         placeholder="Confirm Password"
         bind:value={confirmPassword}
