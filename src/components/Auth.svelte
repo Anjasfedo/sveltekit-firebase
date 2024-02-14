9376a2ccbd6057d33c4cf31f411d7916d3b78525
Anjasfedo <fedoaf98@gmail.com>
2024-02-14 19:50:17 +0700

    Basic Layout

---
+++ src/components/Auth.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-col items-center">
+  <form action="">
+    <h1>Login</h1>
+    <label for=""><input type="text" placeholder="email" /></label>
+    <label for=""><input type="text" placeholder="email" /></label>
+    <label for=""><input type="text" placeholder="email" /></label>
+  </form>
+</div>
